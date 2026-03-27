41b42b2e5d3779ed909573803074f3da997383f8
001Maska <masud.khalif@outlook.com>
2026-03-27 18:33:24 +0300

    Initial Commit

---
+++ Adhkar Ai/Adhkar_AiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adhkar_AiApp.swift
+//  Adhkar Ai
+//
+//  Created by MASKA on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Adhkar_AiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
